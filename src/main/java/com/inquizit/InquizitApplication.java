98ec84974954bbd547a5b0321a21f1e2eb7ace22
mariah.zm <mariah.zammit.18@um.edu.mt>
2022-07-15 09:55:35 +0200

    Initial Spring Project

---
+++ src/main/java/com/inquizit/InquizitApplication.java
@@ -0,0 +1,13 @@
+package com.inquizit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InquizitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InquizitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inquizit/InquizitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inquizit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InquizitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
